9989faad563b7512698021de36d5af21fc408a8c
anik0810 <anikdutta0810@gmail.com>
2023-03-25 17:40:34 +0530

    changed the api to fast api

--- app.py
+++ app.py
@@ -3,11 +3,5 @@ sys.path.append('C:/Users/anikd/virtusa-bot/controller')
 
 from suggestion import *
 
-from flask import Flask,request
-
-app = Flask(__name__)
-
-getSuggestionByLocation('kolkata')
-
 if __name__ == '__main__':
     app.run();
