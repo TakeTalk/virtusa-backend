03cc3961d2435d638fd7b3613c3af6bcc3d78bf3
anik0810 <anikdutta0810@gmail.com>
2023-03-26 07:24:38 +0530

    app.py is configured

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ import sys
 import uvicorn
 sys.path.append('C:/Users/anikd/virtusa-bot/controller')
 
-from suggestion import *
+from controller.suggestion import *
 
 if __name__ == '__main__':
-    app.run();
+    runApplication()
 
 # currently we are running the suggestion.py,we are not using this app.py
 
--- controller/suggestion.py
+++ controller/suggestion.py
@@ -1,8 +1,8 @@
 import sys
 from fastapi import FastAPI
 import uvicorn
-sys.path.append('C:/Users/anikd/virtusa-bot/service') 
-from location import *
+
+from service.location import *
 
 app = FastAPI()
  
@@ -14,7 +14,7 @@ def getSuggestionByLocation(address):
 def getSuggestionByLatLong(lat,longt):
     return fetchByLatLong(lat,longt)
 
-if __name__ == '__main__':
-    uvicorn.run(app, host='localhost', port=5000)
+def runApplication():
+    uvicorn.run(app, host='127.0.0.1', port=5000)
 
 
