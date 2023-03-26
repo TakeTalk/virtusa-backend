ecc94681a0b981e2ae686ab56c49211ac7ff147e
anik0810 <anikdutta0810@gmail.com>
2023-03-26 06:42:42 +0530

    added requirements.txt

--- app.py
+++ app.py
@@ -4,5 +4,8 @@ sys.path.append('C:/Users/anikd/virtusa-bot/controller')
 
 from suggestion import *
 
+if __name__ == '__main__':
+    app.run();
+
 # currently we are running the suggestion.py,we are not using this app.py
 
