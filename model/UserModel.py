5baecd751b7bfba624ccf25d7b17704711f6b38e
anik0810 <anikdutta0810@gmail.com>
2023-04-09 22:46:53 +0530

    changes for login

--- model/UserModel.py
+++ model/UserModel.py
@@ -2,9 +2,8 @@ from pydantic import BaseModel
 class UserBody(BaseModel):
     email: str
     phone: str | None = None
-    firstName: str
-    middleName: str | None = None
-    lastName: str
+    name: str
+    photoUrl: str
     rewardsPoints: int
 
 
