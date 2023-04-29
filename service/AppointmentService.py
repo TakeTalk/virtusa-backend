675b826536b7bf43d0efe61888d59e0b64ce250a
arka-2204 <arkaprabhac16@gmail.com>
2023-04-29 16:25:12 +0530

    commit message

--- service/AppointmentService.py
+++ service/AppointmentService.py
@@ -6,13 +6,13 @@ from selenium.webdriver.common.by import By
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=options)
-driver.get('https://www.apollohospitals.com/kolkataapolloconsultleads/')
-
-
-username = driver.find_element(By.ID,'txtYourName')
+driver.get('https://www.apollo247.com/appointment/index.html')
+username = driver.find_element(By.ID,'firstName')
 selectCity = driver.find_element(By.ID,'city')
+phone_no = driver.find_element(By.ID,'phone')
 
-username.send_keys("you@email.com")
-# password.send_keys("yourpassword")
-# Step 4) Click Login
-# submit.click()
+username.send_keys("you@email.com") #mail
+selectCity.send_keys("kolkata") #city Location
+phone_no.send_keys("9991115554") #Number from user
+btn = driver.find_element(By.ID,'submit')
+btn.click()
